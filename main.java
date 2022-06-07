cb466d71d350d3f525356e40f88a77ef83359497
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-07 16:17:54 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int sayi = input.nextInt();
+        int total = 0, basNumber = 0
+        while(number != 0) {
+
+            total += (number % 10);
+            number /= 10;
+            basNumber++;
+        }
+        System.out.print("Basamak Sayılarının Toplamı :" + total);
+        System.out.print("Basamak Sayılarının Toplamı :" + basNumber);
+    }
+
+    
+}
